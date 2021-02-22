5c91078de13e22f901cc5fb26494497d73a53249
Sergey Tihon <sergey.tihon@gmail.com>
2021-02-22 22:38:47 +0100

    dependencies update

--- src/extension.fs
+++ src/extension.fs
@@ -6,7 +6,7 @@ let activate (context : vscode.ExtensionContext) =
     printfn "Hello world"
 
     let action : obj -> obj = fun _ ->
-        vscode.window.showInformationMessage("Hello world!", Array.empty<string>)
+        vscode.window.showInformationMessage("Hello world!", Array.empty<string>) |> box
 
     vscode.commands.registerCommand("extension.sayHello", action)
     |> context.subscriptions.Add
